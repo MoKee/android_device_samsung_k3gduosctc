cfc73bcc88e466578a3dbf322dad0c576263dabd
GaHoKwan <kwangaho@gmail.com>
2016-10-13 15:33:53 +0800

    Revert "k3gduosctc: Samsung DualModem Features"
    This reverts commit 486dd7aff3c57c0165a8cec7aa76004fefe777b7.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,14 +2,6 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := cpboot-daemon
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := etc/sbin/cbd
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
