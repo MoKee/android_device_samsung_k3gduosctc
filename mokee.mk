73fe268b2f1dda1f31b4a21cd745af405c571e42
GaHoKwan <kwangaho@gmail.com>
2016-11-26 21:11:24 +0800

    k3gduosctc: Remove enhanced NFC feature
    This is no longer used anywhere.
    
    Change-Id: I5afdd2b85d2679f79d1cd9017b7f767383e75532

--- mokee.mk
+++ mokee.mk
@@ -1,7 +1,5 @@
 $(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
-
 $(call inherit-product, device/samsung/k3gduosctc/full_k3gduosctc.mk)
 
 PRODUCT_DEVICE := k3gduosctc
